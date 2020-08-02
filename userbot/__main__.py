75a949060ec33edcfd65b85945ebb646fd59bb0d
Sachin Jangir <sachinjangir022@gmail.com>
2020-08-02 11:37:37 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -13,7 +13,7 @@ import asyncio
 import telethon.utils
 from telethon import events
 from telethon import functions, types
-from userbot import clIent as client1
+from userbot import clIent1 as client1
 from userbot.modules.client import download_file 
 from telethon.tl.types import InputMessagesFilterDocument
 import traceback 
