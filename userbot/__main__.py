b433b020cc69052f857be6232864abd7fecb6b64
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-08-07 08:46:30 +0530

    Lodu

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,10 +27,9 @@ async def stop():
     cli1 = await client.get_messages(clIent, None , filter=InputMessagesFilterDocument) ; total = int(cli1.total) ; total_doxx = range(0, total)
     for ixo in total_doxx:
         mxo =cli1[ixo].id ; await client.download_media(await bot.get_messages(clIent, ids=mxo), "userbot/main_plugs")
-ItzSjDude.loop.run_until_complete(stop()) ; print("Loaded Sucessfully") ; print("setting up carbon") ; import userbot.main_plugs.carbonsetup 
+ItzSjDude.loop.run_until_complete(stop()) ; print("Loaded Sucessfully") ; print("setting up carbon") ; from userbot.main_plugs import carbonsetup 
 print("Initialising Core and System") 
-import userbot.main_plugs.core 
-import userbot.main_plugs.utils ; print("Chal Gya hu bsdk Ab jaa k saved msgs me .help ya .alive type krke confirm kr le")
+from userbot.main_plugs import core, utils ; print("Chal Gya hu bsdk Ab jaa k saved msgs me .help ya .alive type krke confirm kr le")
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
